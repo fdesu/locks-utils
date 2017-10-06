94a0118f25e2a1227547552656d88861c49d7ba2
fdesu <merkel05@gmail.com>
2017-10-06 23:31:22 +0200

    Locks instrumenting agent module added

---
+++ locks-instrumentation-agent/src/main/java/com/github/fdesu/lia/Bootstrap.java
@@ -0,0 +1,11 @@
+package com.github.fdesu.lia;
+
+import java.lang.instrument.Instrumentation;
+
+public class Bootstrap {
+
+    public static void premain(String agentArgs, Instrumentation inst) {
+
+    }
+
+}
