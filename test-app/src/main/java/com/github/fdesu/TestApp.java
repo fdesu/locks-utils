eed7f0a7722be8c4c5a4cd1968f8409d05c9a2ed
fdesu <merkel05@gmail.com>
2017-10-07 23:38:55 +0200

    Enhancement for TestApp

--- test-app/src/main/java/com/github/fdesu/TestApp.java
+++ test-app/src/main/java/com/github/fdesu/TestApp.java
@@ -1,9 +1,25 @@
 package com.github.fdesu;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TestApp {
+    private static final Lock LOCK = new ReentrantLock();
 
     public static void main(String[] args) {
-        System.out.println("Test app has been launched!");
+        print();
     }
+
+    private static synchronized void print() {
+        synchronized (TestApp.class) {
+            try {
+                LOCK.lock();
+                System.out.println("Test app has been launched!");
+            } finally {
+                LOCK.unlock();
+            }
+        }
+    }
+
 
 }
